e7e29912b4a4649c9b39ad277860990084dd0f82
Seif Hendawy <108102491+Hendawyy@users.noreply.github.com>
2023-09-22 18:05:29 +0300

    Update installNginx.groovy

--- vars/installNginx.groovy
+++ vars/installNginx.groovy
@@ -1,9 +1,20 @@
-#!/usr/bin/env/groovy
 def call() {
-    node {
+    node('AWS') {
         stage('Install Nginx') {
-            sh 'sudo apt update'
-            sh 'sudo apt install nginx -y'
+            steps {
+                sh 'sudo apt-get install nginx -y'
+            }
+        }
+        stage('Enable Firewall') {
+            steps {
+                sh "sudo ufw allow 'Nginx HTTP' -y"
+                sh 'sudo rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default /var/www/html/index.nginx-debian.html'
+            }
+        }
+        stage('Test Nginx') {
+            steps {
+                sh 'sudo systemctl status nginx'
+            }
         }
     }
 }
